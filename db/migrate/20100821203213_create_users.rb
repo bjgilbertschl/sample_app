b811918c1e6ce20a82972dc998cace8b3119c171
EC2 Default User <ec2-user@ip-10-112-47-134.ec2.internal>
2013-11-20 07:12:16 +0000

    Modified database script

--- db/migrate/20100821203213_create_users.rb
+++ db/migrate/20100821203213_create_users.rb
@@ -1,6 +1,6 @@
 class CreateUsers < ActiveRecord::Migration
   def self.up
-    create_table :users do |t|
+    create_table :users do |t| #making a change
       t.string :name
       t.string :email
 
